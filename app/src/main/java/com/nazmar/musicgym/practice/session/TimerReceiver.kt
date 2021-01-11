acfad2edb88f74f8c0d5636f18e459d80be8b2a5
Ramzan Sheikh <s.ramzan@protonmail.com>
2021-01-11 17:11:15 -0500

    TimerService: add timer controls to notification

---
+++ app/src/main/java/com/nazmar/musicgym/practice/session/TimerReceiver.kt
@@ -0,0 +1,15 @@
+package com.nazmar.musicgym.practice.session
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class TimerReceiver(private val timer: TimerService.Timer) : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            RESUME_TIMER -> timer.startTimer()
+            PAUSE_TIMER -> timer.pauseTimer()
+            RESTART_TIMER -> timer.restartTimer()
+        }
+    }
+}
--- app/src/main/java/com/nazmar/musicgym/practice/session/TimerService.kt
+++ app/src/main/java/com/nazmar/musicgym/practice/session/TimerService.kt
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.media.MediaPlayer
 import android.media.RingtoneManager
 import android.os.*
@@ -15,8 +16,14 @@ import androidx.lifecycle.Transformations
 import com.nazmar.musicgym.MainActivity
 import com.nazmar.musicgym.R
 import com.nazmar.musicgym.TimerState
+import java.util.*
+import androidx.media.app.NotificationCompat as MediaNotificationCompat
+
 
 const val TIMER_NOTIFICATION_ID = 0
+const val RESUME_TIMER = "resume_timer"
+const val PAUSE_TIMER = "pause_timer"
+const val RESTART_TIMER = "restart_timer"
 
 class TimerService : Service() {
 
@@ -24,41 +31,115 @@ class TimerService : Service() {
 
     private lateinit var notificationManager: NotificationManager
 
-    private lateinit var notification: NotificationCompat.Builder
+    private lateinit var timer: TimerService.Timer
+
+    private lateinit var timerReceiver: TimerReceiver
+
+    private lateinit var pausedNotification: NotificationCompat.Builder
+
+    private lateinit var runningNotification: NotificationCompat.Builder
+
 
     override fun onCreate() {
         super.onCreate()
+
         val sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         mediaPlayer = MediaPlayer.create(this, sound)
         notificationManager = application.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        val contentPendingIntent = PendingIntent.getActivity(
-                this.applicationContext,
+        val contentPendingIntent: PendingIntent = PendingIntent.getActivity(
+                this,
                 TIMER_NOTIFICATION_ID,
-                Intent(this.applicationContext, MainActivity::class.java),
+                Intent(this, MainActivity::class.java),
                 PendingIntent.FLAG_UPDATE_CURRENT
         )
 
-        notification = NotificationCompat.Builder(this, application.getString(R.string.timer_notification_channel_id))
+        val restartPendingIntent: PendingIntent = PendingIntent.getBroadcast(
+                this,
+                0,
+                Intent(RESTART_TIMER),
+                PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val pausePendingIntent: PendingIntent = PendingIntent.getBroadcast(
+                this,
+                1,
+                Intent(PAUSE_TIMER),
+                PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val resumePendingIntent: PendingIntent = PendingIntent.getBroadcast(
+                this,
+                2,
+                Intent(RESUME_TIMER),
+                PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val restartAction = NotificationCompat.Action.Builder(
+                R.drawable.ic_baseline_replay_24,
+                getString(R.string.restart_timer),
+                restartPendingIntent).build()
+        val pauseAction = NotificationCompat.Action.Builder(
+                R.drawable.ic_baseline_pause_24,
+                getString(R.string.pause_timer),
+                pausePendingIntent).build()
+        val resumeAction = NotificationCompat.Action.Builder(
+                R.drawable.ic_baseline_play_arrow_24,
+                getString(R.string.start_timer),
+                resumePendingIntent).build()
+
+        runningNotification = NotificationCompat.Builder(this, application.getString(R.string.timer_notification_channel_id))
                 .setTicker(application.getString(R.string.app_name))
                 .setSmallIcon(R.drawable.ic_baseline_music_note_24)
                 .setOngoing(true)
                 .setContentIntent(contentPendingIntent)
+                .setSubText("Routine Name")
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .addAction(pauseAction)
+                .addAction(restartAction)
+                .setStyle(MediaNotificationCompat.MediaStyle()
+                        .setShowActionsInCompactView(0, 1)
+                )
+
+        pausedNotification = NotificationCompat.Builder(this, application.getString(R.string.timer_notification_channel_id))
+                .setTicker(application.getString(R.string.app_name))
+                .setSmallIcon(R.drawable.ic_baseline_music_note_24)
+                .setOngoing(true)
+                .setContentIntent(contentPendingIntent)
+                .setSubText("Routine Name")
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .addAction(resumeAction)
+                .addAction(restartAction)
+                .setStyle(MediaNotificationCompat.MediaStyle()
+                        .setShowActionsInCompactView(0, 1)
+                )
+
+        timer = Timer()
+        timerReceiver = TimerReceiver(timer)
+        IntentFilter().apply {
+            addAction(RESUME_TIMER)
+            addAction(PAUSE_TIMER)
+            addAction(RESTART_TIMER)
+        }.also {
+            registerReceiver(timerReceiver, it)
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        startForeground(TIMER_NOTIFICATION_ID, notification.build())
+        startForeground(TIMER_NOTIFICATION_ID, runningNotification.build())
         return START_NOT_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        unregisterReceiver(timerReceiver)
         timer.clearTimer()
         notificationManager.cancelAll()
     }
 
     override fun onTaskRemoved(rootIntent: Intent?) {
         super.onTaskRemoved(rootIntent)
+        unregisterReceiver(timerReceiver)
         stopSelf()
     }
 
@@ -68,16 +149,23 @@ class TimerService : Service() {
 
     private val binder = TimerBinder()
 
-    private val timer = Timer()
-
     inner class TimerBinder : Binder() {
         fun getTimer(): Timer = this@TimerService.timer
     }
 
     inner class Timer {
 
+        private var notification = runningNotification
+
         private fun updateTimerNotification() {
             notification.setContentTitle(currentExerciseName)
+            notification.setContentText("Time remaining: ${timeString.value}")
+            notificationManager.notify(TIMER_NOTIFICATION_ID, notification.build())
+        }
+
+        private fun showTimeUpNotification() {
+            notification.setContentTitle(currentExerciseName)
+            notification.setContentText("Time's up!")
             notificationManager.notify(TIMER_NOTIFICATION_ID, notification.build())
         }
 
@@ -111,20 +199,20 @@ class TimerService : Service() {
             timer = object : CountDownTimer(timeLeft.value ?: currentExerciseDuration, 1000) {
                 override fun onTick(millisUntilFinished: Long) {
                     _timeLeft.value = millisUntilFinished
-                    notification.setContentText("Time remaining: ${timeToString(millisUntilFinished)}")
                     updateTimerNotification()
                 }
 
                 override fun onFinish() {
                     timer = null
                     _timeLeft.value = null
-                    notification.setContentText("Time's up!")
-                    updateTimerNotification()
+                    notification = pausedNotification
+                    showTimeUpNotification()
                     mediaPlayer.start()
                     _timerStatus.value = TimerState.COMPLETED
                 }
             }
             _timeLeft.value = timeLeft.value ?: currentExerciseDuration
+            updateTimerNotification()
         }
 
         fun setUpTimer(newIndex: Int, newExerciseDuration: Long, newExerciseName: String) {
@@ -142,12 +230,14 @@ class TimerService : Service() {
 
         fun startTimer() {
             timer?.let {
+                notification = runningNotification
                 it.start()
                 _timerStatus.value = TimerState.RUNNING
             }
         }
 
         fun pauseTimer() {
+            notification = pausedNotification
             timer?.cancel()
             createTimer()
             _timerStatus.value = TimerState.PAUSED
@@ -162,6 +252,7 @@ class TimerService : Service() {
                     startTimer()
                 } else {
                     _timerStatus.value = TimerState.PAUSED
+                    updateTimerNotification()
                 }
             } else {
                 _timerStatus.value = TimerState.STOPPED
