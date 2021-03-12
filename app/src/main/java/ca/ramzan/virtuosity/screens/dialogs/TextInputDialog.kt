09a77b7aacaeab6be7800a1c12d7a1d2b7b53295
Ramzan Sheikh <s.ramzan@protonmail.com>
2021-03-12 16:52:08 -0500

    TextInputDialog: fix keyboard not showing on some devices

--- app/src/main/java/ca/ramzan/virtuosity/screens/dialogs/TextInputDialog.kt
+++ app/src/main/java/ca/ramzan/virtuosity/screens/dialogs/TextInputDialog.kt
@@ -5,17 +5,17 @@ import android.app.Dialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import ca.ramzan.virtuosity.R
 import ca.ramzan.virtuosity.common.FIRST_RUN_KEY
 import ca.ramzan.virtuosity.common.INPUT_TEXT
 import ca.ramzan.virtuosity.common.TEXT_INPUT_RESULT
-import ca.ramzan.virtuosity.common.showKeyboard
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -68,8 +68,9 @@ class TextInputDialog : DialogFragment() {
             }
         })
 
+        dialog.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+
         dialog.setOnShowListener {
-            imm.showKeyboard()
             text.requestFocus()
             text.setSelection(text.text.length)
             dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = text.text.isNotEmpty()
