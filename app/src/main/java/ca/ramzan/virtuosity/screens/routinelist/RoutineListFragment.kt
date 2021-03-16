19778faa5545f2e89fbeac03ca80c3ae1651b84f
Ramzan Sheikh <s.ramzan@protonmail.com>
2021-03-16 14:57:13 -0400

    RoutineList: change viewmodel scope from activityviewmodels to viewmodels to fix previews not refreshing

--- app/src/main/java/ca/ramzan/virtuosity/screens/routinelist/RoutineListFragment.kt
+++ app/src/main/java/ca/ramzan/virtuosity/screens/routinelist/RoutineListFragment.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
@@ -24,7 +24,7 @@ class RoutineListFragment : BaseFragment<FragmentRoutineListBinding>() {
     @Inject
     lateinit var prefs: SharedPreferences
 
-    private val viewModel: RoutineListViewModel by activityViewModels()
+    private val viewModel: RoutineListViewModel by viewModels()
 
     override fun onStart() {
         super.onStart()
