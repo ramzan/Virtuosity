6b185444db594c89dd231ab13b0056a09d8aec29
Ramzan Sheikh <s.ramzan@protonmail.com>
2021-04-16 15:55:17 -0400

    Settings: add bottom padding to account for bottom navigation view

--- app/src/main/java/ca/ramzan/virtuosity/screens/settings/SettingsFragment.kt
+++ app/src/main/java/ca/ramzan/virtuosity/screens/settings/SettingsFragment.kt
@@ -1,6 +1,10 @@
 package ca.ramzan.virtuosity.screens.settings
 
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import ca.ramzan.virtuosity.R
@@ -11,6 +15,25 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onStart() {
         super.onStart()
         requireActivity().showBottomNavBar()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState).apply {
+            // Setting bottom padding to account for the bottom navigation view
+            val tv = TypedValue()
+            if (requireActivity().theme.resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+                this?.setPadding(
+                    0,
+                    0,
+                    0,
+                    TypedValue.complexToDimensionPixelSize(tv.data, resources.displayMetrics)
+                )
+            }
+        }
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
