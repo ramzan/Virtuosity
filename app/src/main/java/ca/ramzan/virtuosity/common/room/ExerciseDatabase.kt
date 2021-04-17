77f5d13c50bc846dd56eeda4c2c60308fee83ac5
Ramzan Sheikh <s.ramzan@protonmail.com>
2021-04-17 18:29:08 -0400

    Database: add starter database and export schema

--- app/src/main/java/ca/ramzan/virtuosity/common/room/ExerciseDatabase.kt
+++ app/src/main/java/ca/ramzan/virtuosity/common/room/ExerciseDatabase.kt
@@ -5,17 +5,12 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.sqlite.db.SupportSQLiteDatabase
 import ca.ramzan.virtuosity.exercises.Exercise
 import ca.ramzan.virtuosity.exercises.ExerciseHistory
 import ca.ramzan.virtuosity.history.SessionHistoryEntity
 import ca.ramzan.virtuosity.routine.Routine
 import ca.ramzan.virtuosity.routine.RoutineExerciseEntity
 import ca.ramzan.virtuosity.session.SessionExercise
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @Database(
     entities = [
@@ -27,7 +22,7 @@ import kotlinx.coroutines.withContext
         SessionHistoryEntity::class
     ],
     version = 1,
-    exportSchema = false
+    exportSchema = true
 )
 @TypeConverters(Converters::class)
 abstract class ExerciseDatabase : RoomDatabase() {
@@ -53,105 +48,13 @@ abstract class ExerciseDatabase : RoomDatabase() {
                         ExerciseDatabase::class.java,
                         "exercise_database"
                     )
+                        .createFromAsset("db/exercise_database")
                         .fallbackToDestructiveMigration()
-                        .addCallback(object : Callback() {
-                            override fun onCreate(db: SupportSQLiteDatabase) {
-                                super.onCreate(db)
-                                GlobalScope.launch(Dispatchers.IO) {
-                                    withContext(Dispatchers.IO) {
-                                        getInstance(context).run {
-                                            PREPOPULATE_EXERCISES.forEach {
-                                                exerciseListDao.insert(
-                                                    it
-                                                )
-                                            }
-                                            PREPOPULATE_ROUTINES.forEach {
-                                                routineEditorDao.insert(
-                                                    it
-                                                )
-                                            }
-                                            routineEditorDao.insertRoutineExercises(
-                                                PREPOPULATE_ROUTINE_EXERCISES
-                                            )
-                                        }
-                                    }
-                                }
-                            }
-                        })
                         .build()
                     INSTANCE = instance
                 }
                 return instance
             }
         }
-
-        val PREPOPULATE_EXERCISES = listOf(
-            Exercise("A Major Scale"),
-            Exercise("A# Major Scale"),
-            Exercise("B Major Scale"),
-            Exercise("C Major Scale"),
-            Exercise("C# Major Scale"),
-            Exercise("D Major Scale"),
-            Exercise("D# Major Scale"),
-            Exercise("E Major Scale"),
-            Exercise("F Major Scale"),
-            Exercise("F# Major Scale"),
-            Exercise("G Major Scale"),
-            Exercise("G# Major Scale"),
-            Exercise("A Minor Scale"),
-            Exercise("A# Minor Scale"),
-            Exercise("B Minor Scale"),
-            Exercise("C Minor Scale"),
-            Exercise("C# Minor Scale"),
-            Exercise("D Minor Scale"),
-            Exercise("D# Minor Scale"),
-            Exercise("E Minor Scale"),
-            Exercise("F Minor Scale"),
-            Exercise("F# Minor Scale"),
-            Exercise("G Minor Scale"),
-            Exercise("G# Minor Scale"),
-            Exercise("A Harmonic Minor Scale"),
-            Exercise("A# Harmonic Minor Scale"),
-            Exercise("B Harmonic Minor Scale"),
-            Exercise("C Harmonic Minor Scale"),
-            Exercise("C# Harmonic Minor Scale"),
-            Exercise("D Harmonic Minor Scale"),
-            Exercise("D# Harmonic Minor Scale"),
-            Exercise("E Harmonic Minor Scale"),
-            Exercise("F Harmonic Minor Scale"),
-            Exercise("F# Harmonic Minor Scale"),
-            Exercise("G Harmonic Minor Scale"),
-            Exercise("G# Harmonic Minor Scale")
-        )
-
-        val PREPOPULATE_ROUTINES = listOf(
-            Routine("A", 1),
-            Routine("B"),
-            Routine("C"),
-            Routine("D"),
-            Routine("E"),
-            Routine("F"),
-            Routine("G"),
-            Routine("H"),
-            Routine("I"),
-            Routine("J"),
-            Routine("L"),
-            Routine("M"),
-            Routine("N"),
-            Routine("O"),
-            Routine("p"),
-            Routine("Q"),
-            Routine("r"),
-            Routine("s"),
-            Routine("T"),
-        )
-
-        val PREPOPULATE_ROUTINE_EXERCISES = listOf(
-            RoutineExerciseEntity(1, 1, 1, 81000),
-            RoutineExerciseEntity(1, 2, 2, 72000),
-            RoutineExerciseEntity(1, 3, 3, 63000),
-            RoutineExerciseEntity(1, 4, 9, 54000),
-            RoutineExerciseEntity(1, 5, 2, 45000)
-        )
     }
 }
